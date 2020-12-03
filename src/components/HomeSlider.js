fd1abd63cc11e44ba897faf1fca4195ec87d9f7e
Uduak Essien <acushla4real@gmail.com>
2020-12-03 20:18:56 +0100

    Add HomeSlider Component and image assets

---
+++ src/components/HomeSlider.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import Carousel from 'react-bootstrap/Carousel'
+import slide001 from '../assets/images/banner/slide001.jpg'
+import slide002 from '../assets/images/banner/slide002.jpg'
+import slide003 from '../assets/images/banner/slide003_boyreading.jpg'
+
+export default function HomeSlider() {
+    const [index, setIndex] = useState(0);
+  
+    const handleSelect = (selectedIndex, e) => {
+      setIndex(selectedIndex);
+    };
+  
+    return (
+      <Carousel activeIndex={index} onSelect={handleSelect}>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={slide001}
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={slide002}
+            alt="Second slide"
+          />
+  
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={slide003}
+            alt="Third slide"
+          />
+  
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    );
+  }
